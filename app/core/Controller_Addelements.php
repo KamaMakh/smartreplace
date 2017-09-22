1c8834cee06c9bd7237100675b62791aeeb3c40f
Kamron <meganyuton1@gmail.com>
2017-09-22 12:30:28 +0500

    Initial commit

--- app/core/Controller_Addelements.php
+++ app/core/Controller_Addelements.php
@@ -16,14 +16,25 @@ class Controller_Addelements
     function init() {
         global $smarty;
         if ( isset($_GET['site_url']) ) {
+
             $this->site_url = $_GET['site_url'];
-            $smarty->assign('domain', $this->site_url);
+
+            $page = file_get_contents($this->site_url);
+
+            $page = str_replace('href=', 'href="'.$this->site_url, $page);
+            $page = str_replace($this->site_url.'"', $this->site_url, $page);
+
+            $page = str_replace('src=', 'src="'.$this->site_url, $page);
+            $page = str_replace($this->site_url.'"/', $this->site_url, $page);
+
+            file_put_contents('html_text.txt', $page);
+
             $smarty->assign('iframe', '<iframe class="main_iframe" id="main_iframe" src="http://megayagla.local/addelements/getcontent" frameborder="0" width="100%" height="600px"></iframe>');
             $smarty->display('addelements.tpl');
         }
     }
 
     public function getcontent () {
-
+        echo file_get_contents('html_text.txt');
     }
 }
