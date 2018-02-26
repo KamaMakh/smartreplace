0e3268cc03fd66fd5e5df8ff5399c8d55127cdc5
Kamron <meganyuton1@gmail.com>
2018-02-26 18:40:46 +0500

    - исправлен скрипт

--- public/static/js/client/sr.service.js
+++ public/static/js/client/sr.service.js
@@ -1,64 +1,65 @@
 window.addEventListener('DOMContentLoaded', function() {
 
-    let preloader = document.createElement('div');
-    preloader.setAttribute("class","preload");
-    document.body.appendChild(preloader);
+    if ( location.search.indexOf('yagla=true') ){
+        let preloader = document.createElement('div');
+        preloader.setAttribute("class","preload");
+        document.body.appendChild(preloader);
 
-    const utilities = {
-        htmlspecialchars_decode(data) {
-            data = data.replace(/&quot;/g, '"');
-            data = data.replace(/&lt;/g, '<');
-            data = data.replace(/&gt;/g, '>');
-            return data;
-        }
-    };
+        const utilities = {
+            htmlspecialchars_decode(data) {
+                data = data.replace(/&quot;/g, '"');
+                data = data.replace(/&lt;/g, '<');
+                data = data.replace(/&gt;/g, '>');
+                return data;
+            }
+        };
 
-    const module = {
+        const module = {
 
-        getUrl() {
-            let project_name = document.location.origin,
-                get_params = document.location.search;
+            getUrl() {
+                let project_name = document.location.origin,
+                    get_params = document.location.search;
 
-            get_params = get_params.split('?')[1];
-            if (get_params) {
-                get_params = get_params.split('&');
-                get_params = get_params.map(function(item){
-                    if (item.split('=')[0] == 'sr') {
-                        return item;
+                get_params = get_params.split('?')[1];
+                if (get_params) {
+                    get_params = get_params.split('&');
+                    get_params = get_params.map(function(item){
+                        if (item.split('=')[0] == 'sr') {
+                            return item;
+                        }
+                    });
+
+                    if (get_params[0]) {
+                        get_params = get_params[0].split('=')[1].split('s');
+
+                        return {
+                            group_id: get_params[0],
+                            project_id: get_params[1],
+                            project_name: project_name
+                        }
                     }
-                });
 
-                if (get_params[0]) {
-                    get_params = get_params[0].split('=')[1].split('s');
-
-                    return {
-                        group_id: get_params[0],
-                        project_id: get_params[1],
-                        project_name: project_name
-                    }
+                } else {
+                    return false;
                 }
 
-            } else {
-                return false;
-            }
+            },
 
-        },
+            requestParam(data) {
+                const url = `http://kamron.webx.brn.m/srapi?mode=getGroup&group_id=${data['group_id']}&project_id=${data['project_id']}`;
 
-        requestParam(data) {
-            const url = `http://kamron.webx.brn.m/srapi?mode=getGroup&group_id=${data['group_id']}&project_id=${data['project_id']}`;
+                let XHR = ("onload" in new XMLHttpRequest()) ? XMLHttpRequest : XDomainRequest;
 
-            let XHR = ("onload" in new XMLHttpRequest()) ? XMLHttpRequest : XDomainRequest;
+                let xhr = new XHR();
 
-            let xhr = new XHR();
+                // (2) запрос на другой домен :)
+                xhr.open('GET', url, true);
 
-            // (2) запрос на другой домен :)
-            xhr.open('GET', url, true);
+                xhr.onload = function() {
+                    // console.log( this.responseText );
+                    let text = utilities.htmlspecialchars_decode(this.responseText);
 
-            xhr.onload = function() {
-               // console.log( this.responseText );
-                let text = utilities.htmlspecialchars_decode(this.responseText);
-
-                let elements = JSON.parse(text);
+                    let elements = JSON.parse(text);
                     //console.log(elements);
                     for (let key in elements) {
 
@@ -73,29 +74,31 @@ window.addEventListener('DOMContentLoaded', function() {
                         }
 
                     }
-            };
+                };
 
-            xhr.onerror = function() {
-                console.log( 'Ошибка ' + this.status );
-            };
+                xhr.onerror = function() {
+                    console.log( 'Ошибка ' + this.status );
+                };
 
-            xhr.send();
+                xhr.send();
 
-        },
+            },
 
-        run() {
-            let get_params = this.getUrl();
+            run() {
+                let get_params = this.getUrl();
 
-            if (get_params) {
-               // this.check_script(get_params);
-                this.requestParam(get_params);
+                if (get_params) {
+                    // this.check_script(get_params);
+                    this.requestParam(get_params);
+                }
+
             }
+        };
+        setTimeout(function(){
+            module.run();
+        },100);
+    }
 
-        }
-    };
-    setTimeout(function(){
-        module.run();
-    },100);
 
 
     //----------------------------------------------------------------------
